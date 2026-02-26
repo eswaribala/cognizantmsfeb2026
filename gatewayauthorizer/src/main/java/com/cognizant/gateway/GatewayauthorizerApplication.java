4103d4e9e8ccf59e8ce7669b5eb41b1b83858468
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-26 17:19:15 +0530

    updated

---
+++ gatewayauthorizer/src/main/java/com/cognizant/gateway/GatewayauthorizerApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayauthorizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayauthorizerApplication.class, args);
+	}
+
+}
