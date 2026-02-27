6fd0b1473bc1d3ff4b79dc8545d4c9c1a3c1cf69
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-27 12:45:59 +0530

    updated

--- customerservice/src/main/java/com/cognizant/customerservice/entities/Customer.java
+++ customerservice/src/main/java/com/cognizant/customerservice/entities/Customer.java
@@ -1,8 +1,14 @@
 package com.cognizant.customerservice.entities;
 
+import com.cognizant.customerservice.facades.AccountNoAnnotator;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,13 +18,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "customer")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Customer {
 	@Id
+	@AccountNoAnnotator
+	@Column(name = "account_no")
+	@Schema(hidden = true)
 	private long accountNo;
 	@Embedded
 	private FullName fullName;
+	@Column(name = "contact_no")
 	private long contactNo;
+	@Column(name = "email", unique = true, nullable = false,length = 100)
 	private String email;
+	@Column(name = "password", nullable = false,length = 10)
 	private String password;
 	
 
