8c1a8a8af4c57a6de7a62350a1c393eebdd0c956
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-27 15:37:58 +0530

    updated

--- customerservice/src/main/java/com/cognizant/customerservice/dtos/CustomerRequest.java
+++ customerservice/src/main/java/com/cognizant/customerservice/dtos/CustomerRequest.java
@@ -1,5 +1,6 @@
 package com.cognizant.customerservice.dtos;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.NoArgsConstructor;
 public class CustomerRequest {
 	
 	private FullNameRequest fullNameRequest;
+	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email format")
 	private String email;
 	private long contactNo;
+	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$", message = "Password must be at least 8 characters long and contain both letters and numbers")
 	private String password;
 	
 
