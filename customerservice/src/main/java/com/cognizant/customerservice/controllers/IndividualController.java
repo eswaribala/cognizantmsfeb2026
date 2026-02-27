07bc3a29469b438c99fba649a84329327d99cd7d
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-27 17:08:39 +0530

    updated

--- customerservice/src/main/java/com/cognizant/customerservice/controllers/IndividualController.java
+++ customerservice/src/main/java/com/cognizant/customerservice/controllers/IndividualController.java
@@ -1,13 +1,18 @@
 package com.cognizant.customerservice.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.customerservice.dtos.GenericMessage;
 import com.cognizant.customerservice.dtos.IndividualRequest;
+import com.cognizant.customerservice.dtos.IndividualResponse;
+import com.cognizant.customerservice.entities.Individual;
+import com.cognizant.customerservice.mappers.IndividualMapper;
 import com.cognizant.customerservice.services.IndividualService;
 
 @RestController
@@ -15,10 +20,19 @@ import com.cognizant.customerservice.services.IndividualService;
 public class IndividualController {
 	@Autowired
 	private IndividualService individualService;
-	
-	public ResponseEntity<GenericMessage> addIndividual(@RequestBody IndividualRequest individualRequest) {
-		//automapper
-		
+	@Autowired
+	private IndividualMapper individualMapper;
+	@PostMapping("/v1.0")
+	public ResponseEntity<GenericMessage> createIndividual(@RequestBody IndividualRequest individualRequest) {
+		//dto to entity
+		Individual individual = individualMapper.toEntity(individualRequest);
+		Individual savedIndividual = individualService.addIndividual(individual);
+		//entity to dto
+		IndividualResponse individualResponse = 
+				individualMapper.toRequest(savedIndividual);
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(new GenericMessage<IndividualResponse>(individualResponse));
 	}
+	
 
 }
