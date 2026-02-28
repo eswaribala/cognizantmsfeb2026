a01cb5d4faf7988d4827a880e1aa609678496739
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-02-28 16:14:42 +0530

    updated

--- customerservice/src/test/java/com/cognizant/customerservice/dtos/TestCustomerRequest.java
+++ customerservice/src/test/java/com/cognizant/customerservice/dtos/TestCustomerRequest.java
@@ -1,13 +1,23 @@
 package com.cognizant.customerservice.dtos;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.github.javafaker.Faker;
 
 public class TestCustomerRequest {
 	private static CustomerRequest customerRequest;
@@ -35,6 +45,58 @@ public class TestCustomerRequest {
 		customerRequest.setPassword(password);
 		assertTrue(customerRequest.getFullNameRequest()
 				.getFirstName().equals(firstName));	
+		assertAll(() -> assertTrue(customerRequest.getFullNameRequest()
+				.getLastName().equals(lastName)),
+				() -> assertTrue(customerRequest.getEmail().equals(email)),
+				() -> assertTrue(customerRequest.getContactNo() == contactNo),
+				() -> assertTrue(customerRequest.getPassword().equals(password)));		
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"Mithun", "Rakesh", "Swathi"})
+	public void testCustomerRequestFirstName(String name) {
+		fullNameRequest.setFirstName(name);
+		customerRequest.setFullNameRequest(fullNameRequest);
+		assertNotNull(customerRequest.getFullNameRequest().getFirstName());
+	}
+	
+	
+	@ParameterizedTest
+	@MethodSource("getCustomerRequestList")
+	public void testCustomerRequestPropertiesUsingMethodSource(CustomerRequest customerRequest) {
+		fullNameRequest.setFirstName(customerRequest.getFullNameRequest().getFirstName());
+		fullNameRequest.setLastName(customerRequest.getFullNameRequest().getLastName());
+		customerRequest.setFullNameRequest(fullNameRequest);
+		customerRequest.setEmail(customerRequest.getEmail());
+		customerRequest.setContactNo(customerRequest.getContactNo());
+		customerRequest.setPassword(customerRequest.getPassword());
+		assertTrue(customerRequest.getFullNameRequest()
+				.getFirstName().equals(fullNameRequest.getFirstName()));	
+		assertAll(() -> assertTrue(customerRequest.getFullNameRequest()
+				.getLastName().equals(fullNameRequest.getLastName())),
+				() -> assertTrue(customerRequest.getEmail().equals(customerRequest.getEmail())),
+				() -> assertTrue(customerRequest.getContactNo() == customerRequest.getContactNo()),
+				() -> assertTrue(customerRequest.getPassword().equals(customerRequest.getPassword())));		
+	}
+	
+	
+	private static Stream<Arguments> getCustomerRequestList() {
+		
+		List<Arguments> list = new ArrayList<>();
+		Faker faker = new Faker();
+		for(int i=0; i<5; i++) {
+			FullNameRequest fullNameRequest = new FullNameRequest();
+			CustomerRequest customerRequest = new CustomerRequest();
+			fullNameRequest.setFirstName(faker.name().firstName());
+			fullNameRequest.setLastName(faker.name().lastName());
+			customerRequest.setFullNameRequest(fullNameRequest);
+			customerRequest.setEmail(faker.internet().emailAddress());
+			customerRequest.setContactNo(faker.number().numberBetween(1000000000L, 9999999999L));
+			customerRequest.setPassword(faker.internet().password());
+			list.add(Arguments.of(customerRequest));
+		}
+		return list.stream();
+		
 	}
 	
 }
