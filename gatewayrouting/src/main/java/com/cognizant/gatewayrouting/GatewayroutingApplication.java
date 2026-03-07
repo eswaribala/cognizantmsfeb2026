7d47c627a7ae3b08ed5f379e06d2531f4365fabe
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-03-07 16:00:48 +0530

    updated

--- gatewayrouting/src/main/java/com/cognizant/gatewayrouting/GatewayroutingApplication.java
+++ gatewayrouting/src/main/java/com/cognizant/gatewayrouting/GatewayroutingApplication.java
@@ -2,8 +2,10 @@ package com.cognizant.gatewayrouting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayroutingApplication {
 
 	public static void main(String[] args) {
