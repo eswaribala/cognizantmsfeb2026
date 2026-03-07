7bd7fe92c892b53441a348577efe2c602da31ec7
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-03-07 15:53:05 +0530

    updated

---
+++ gatewayrouting/src/main/java/com/cognizant/gatewayrouting/GatewayroutingApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.gatewayrouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayroutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayroutingApplication.class, args);
+	}
+
+}
---
+++ gatewayrouting/src/test/java/com/cognizant/gatewayrouting/GatewayroutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.gatewayrouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayroutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
